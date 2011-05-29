f5bd5769a3677721607ce8607764574f930f092b
Trinity Bays <trinity93@gmail.com>
2011-05-29 00:39:30 -0500

    Opps that cant be in there

--- www/serverstats/sources/external/cacti/3com_cable_modem.pl
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/perl
-
-$status=`/bin/bash -c 'wget --quiet -O - http://192.168.100.1/cgibin/opcfg | grep "10.18.18.11" -c'`;
-chomp $status;
-print $status;
--- www/serverstats/sources/external/cacti/unix_processes.pl
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/perl
-
-$output = `ps ax | grep -c :`;
-chomp($output);
-print $output;
