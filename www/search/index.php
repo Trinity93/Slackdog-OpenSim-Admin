f2de5a2ff7e8db1dde440bfae3f357c48992d860
Trinity Bays <trinity93@gmail.com>
2011-06-26 16:38:04 -0500

    lets get the search file in there

---
+++ www/search/index.php
@@ -0,0 +1,128 @@
+<html>
+<head>
+<title>search script</title>
+<meta name="author" content="slackdog">
+</head>
+
+<body>
+
+<form name="form" action="index.php" method="get">
+  <input type="text" name="q" />
+  <input type="submit" name="Submit" value="Search" />
+</form>
+
+<?php
+
+  // Get the search variable from URL
+
+  $var = $_GET['q'] ;
+  $s   = $_GET['r'] ;
+  $trimmed = trim($var); //trim whitespace from the stored variable
+
+// rows to return
+$limit=10; 
+
+// check for an empty string and display a message.
+if ($trimmed == "")
+  {
+  echo "<p>Please enter a search...</p>";
+  exit;
+  }
+
+// check for a search parameter
+if (!isset($var))
+  {
+  echo "<p>We dont seem to have a search parameter!</p>";
+  exit;
+  }
+
+//connect to your database ** EDIT REQUIRED HERE **
+mysql_connect("localhost","wiredux","*****"); //(host, username, password)
+
+//specify database ** EDIT REQUIRED HERE **
+mysql_select_db("ossearch") or die("Unable to select database"); //select which database we're using
+
+// Build SQL Query  
+$query = "select * from parcels where description like \"%$trimmed%\" or parcelname like \"%$trimmed%\"  
+  order by description"; // EDIT HERE and specify your table and field names for the SQL query
+
+ $numresults=mysql_query($query);
+ $numrows=mysql_num_rows($numresults);
+
+// If we have no results, offer a google search as an alternative
+
+if ($numrows == 0)
+  {
+  echo "<h4>Results</h4>";
+  echo "<p>Sorry, your search: &quot;" . $trimmed . "&quot; returned zero results</p>";
+
+// google
+ echo "<p><a href=\"http://www.google.com/search?q=" 
+  . $trimmed . "\" target=\"_blank\" title=\"Look up 
+  " . $trimmed . " on Google\">Click here</a> to try the 
+  search on google</p>";
+  }
+
+// next determine if s has been passed to script, if not use 0
+  if (empty($s)) {
+  $s=0;
+  }
+
+// get results
+  $query .= " limit $s,$limit";
+  $result = mysql_query($query) or die("Couldn't execute query");
+
+// display what the person searched for
+echo "<p>You searched for: &quot;" . $var . "&quot;</p>";
+
+// begin to show results set
+echo "Results ...</br></br>";
+$count = 1 + $s ;
+
+// now you can display the results returned
+  while ($row= mysql_fetch_array($result)) {
+  $title = $row["parcelname"];
+
+  echo "$count.)&nbsp;$title </br>" ;
+  $count++ ;
+  }
+
+$currPage = (($s/$limit) + 1);
+
+//break before paging
+  echo "<br />";
+
+  // next we need to do the links to other results
+  if ($s>=1) { // bypass PREV link if s is 0
+  $prevs=($s-$limit);
+  print "&nbsp;<a href=\"$PHP_SELF?5=$prevs&q=$var\">&lt;&lt; 
+  Prev 10</a>&nbsp&nbsp;";
+  }
+
+// calculate number of pages needing links
+  $pages=intval($numrows/$limit);
+
+// $pages now contains int of pages needed unless there is a remainder from division
+
+  if ($numrows%$limit) {
+  // has remainder so add one page
+  $pages++;
+  }
+
+// check to see if last page
+  if (!((($s+$limit)/$limit)==$pages) && $pages!=1) {
+
+  // not last page so give NEXT link
+  $news=$s+$limit;
+
+  echo "&nbsp;<a href=\"$PHP_SELF?r=$news&q=$var\">Next 10 &gt;&gt;</a>";
+  }
+
+$a = $s + ($limit) ;
+  if ($a > $numrows) { $a = $numrows ; }
+  $b = $s + 1 ;
+  echo "<p>Showing results $b to $a of $numrows</p>";
+  
+?>
+</body>
+</html>
