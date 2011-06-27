e71ea80ab5e4fbaf88ebcdf48ce0b99f2879f1a7
Riseon Kosten <riseonkosten@gmail.com>
2011-06-27 06:11:21 -0500

    Working on account creation, inserts updated.

--- www/sites/standard/createaccount.php
+++ www/sites/standard/createaccount.php
@@ -484,18 +484,21 @@ else if($_GET[aktion]=="ok")
 						mt_rand( 0, 0x3fff ) | 0x8000,
 						mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ) );
 	
-			$DbLink->query("INSERT INTO ".C_CODES_TBL." (code,UUID,info,email,time)VALUES('$code','$UUID','confirm','$_SESSION[EMAIL]',".time().")");
+                        $DbLink->query("INSERT INTO ".C_CODES_TBL." (code,UUID,info,email,time)VALUES('$code','$UUID','confirm','$_SESSION[EMAIL]',".time().")");
 
-$DbLink->query("INSERT INTO ".C_USERS_TBL." (PrincipalID,FirstName,LastName,created)
-	VALUES
-('$UUID','".$_SESSION['ACCFIRST']."','".$_SESSION['ACCLAST'].','.time());
-	  $DbLink->query("INSERT INTO ".C_AGENTS_TBL." 	(UserID,HomeRegionID,HomePosition,HomeLookAt,LastRegionID,LastPosition,LastLookAt,Online,Login,Logout)
-	VALUES
-('$UUID','".$_SESSION['REGIONID']."','<128,128,128>,'<100,100,100>',".$_SESSION['REGIONID']."','<128,128,128>','<100,100,100>','0',".time());
+$DbLink->query("INSERT INTO ".C_USERS_TBL." (PrincipalID,ScopeID,FirstName,LastName,created)
+        VALUES
+('$UUID','00000000-0000-0000-0000-000000000000','".$_SESSION['ACCFIRST']."','".$_SESSION['ACCLAST']."','".time()."')");
+
+          $DbLink->query("INSERT INTO ".C_AGENTS_TBL."  (UserID,HomeRegionID,HomePosition,HomeLookAt,LastRegionID,LastPosition,LastLookAt,Online,Login,Logout)
+        VALUES
+('$UUID','".$_SESSION['REGIONID']."','<128,128,128>','<100,100,100>',".$_SESSION['REGIONID']."','<128,128,128>','<100,100,100>','false','0','0')");
 
   $DbLink->query("INSERT INTO ".C_AUTH_TBL." (UUID,passwordHash,passwordSalt,webLoginKey,accountType)
-	VALUES
-('$UUID','0','$passwordSalt','00000000-0000-0000-0000-000000000000','UserAccount'");
+        VALUES
+('$UUID','0','$passwordSalt','00000000-0000-0000-0000-000000000000','UserAccount')");
+
+
 
 
 
