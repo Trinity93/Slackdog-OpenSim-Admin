2d198714b89d3ba3703ed7c55c4704fc5afb3a90
Riseon Kosten <riseonkosten@gmail.com>
2011-06-27 09:16:27 -0500

    yay typos

--- sql/webdev.sql
+++ sql/webdev.sql
@@ -18,12 +18,12 @@ CREATE TABLE IF NOT EXISTS `adminsetting` (
   `lastnames` varchar(10) NOT NULL,
   `adress` varchar(32) NOT NULL,
   `region` text NOT NULL,
-  `comfirm18` varchar(32) NOT NULL,
+  `confirm18` varchar(32) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 INSERT INTO `adminsetting` (
-	`startregion`,`userdir`,`griddir`,`assetdir`,`lastnames`,`adress`,`region`)
+	`startregion`,`userdir`,`griddir`,`assetdir`,`lastnames`,`adress`,`region`,`confirm18`)
 	VALUES
 	('0','.','.','.','0','1','0','1') ;
 
